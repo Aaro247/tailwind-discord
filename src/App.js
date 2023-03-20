3655bf010dda9ce5fe8728b5756fc6b07dbba527
Aarohi Ajgaonkar <aarohi@Aarohis-MBP.fritz.box>
2023-03-20 19:51:50 +0100

    feat: content container added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import SideBar from "./components/SideBar";
 import ChannelBar from "./components/ChannelBar";
-import TopNavigation from "./components/TopNavigation";
+import ContentContainer from "./components/ContentContainer";
 
 function App() {
   return (
     <div className="flex">
       <SideBar />
       <ChannelBar />
-      <TopNavigation />
+      <ContentContainer />
     </div>
   );
 }
